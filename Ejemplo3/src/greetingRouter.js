52107932d41a104d6fbcee1ffd1d97aa55e6c493
ivchicano <ivchicano@gmail.com>
2023-09-25 10:42:02 +0200

    add more mustache examples

--- Ejemplo3/src/greetingRouter.js
+++ Ejemplo3/src/greetingRouter.js
@@ -9,4 +9,40 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/cond', (req, res) => {
+    res.render('conditional', {
+        name: "World",
+        hello: true
+    });
+});
+
+router.get('/list', (req, res) => {
+    res.render('lista', {
+        colors: ["red", "green", "blue"]
+    });
+});
+
+router.get('/object', (req, res) => {
+    res.render('object', {
+        name: {
+            first: "Michael",
+            last: "Jackson"
+        }
+    });
+});
+
+router.get('/beatles', (req, res) => {
+    res.render('beatles', {
+        beatles: [
+            { firstName: "John", lastName: "Lennon" },
+            { firstName: "Paul", lastName: "McCartney" },
+            { firstName: "George", lastName: "Harrison" },
+            { firstName: "Ringo", lastName: "Starr" }
+        ],
+        name: function () {
+            return this.firstName + " " + this.lastName;
+        }
+    });
+});
+
 export default router;
