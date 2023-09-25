1e9520f7350416266caa87b4b93ecb1559c83b84
ivchicano <ivchicano@gmail.com>
2023-09-25 11:00:48 +0200

    add form method post example

--- Ejemplo4/src/greetingRouter.js
+++ Ejemplo4/src/greetingRouter.js
@@ -5,7 +5,17 @@ const router = express.Router();
 router.get('/greeting', (req, res) => {
 
     res.render('greeting', {
-        name: req.query.userName
+        name: req.query.userName,
+        country: req.query.country
+    });
+});
+
+router.post('/login', (req, res) => {
+
+    let correctPassword = "1234"
+    res.render('login', {
+        success: req.body.password === correctPassword,
+        name: req.body.userName,
     });
 });
 
